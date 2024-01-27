06d006b3a9945bae026a53c04a97341df32bcfda
shouryade <shourya.de12@gmail.com>
2024-01-28 05:20:00 +0530

    init 2
    Signed-off-by: shouryade <shourya.de12@gmail.com>

--- ezmail/core/apps.py
+++ ezmail/core/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class EzmailConfig(AppConfig):
+class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ezmail'
+    name = 'core'
